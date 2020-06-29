956332fbb416c67d41d1b0c18e317f3b6941caf4
Viktor Osipov <ipovos@pm.me>
2020-06-30 01:52:31 +0300

    Updates

---
+++ precache-manifest.73305bd3ff9c5184ce23b6d15361905b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101d103a0985670c77722bd23b40da51",
+    "url": "/abvoc/index.html"
+  },
+  {
+    "revision": "fb56690342de2a261dee",
+    "url": "/abvoc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f570c7e09d07b46d2c71",
+    "url": "/abvoc/static/js/2.2b31512b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abvoc/static/js/2.2b31512b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb56690342de2a261dee",
+    "url": "/abvoc/static/js/main.f6d02964.chunk.js"
+  },
+  {
+    "revision": "46a332f48d15061fec57",
+    "url": "/abvoc/static/js/runtime-main.a8b1dd07.js"
+  }
+]);
